80d70d0d67f6851d9f0bbe58b488f45679444332
Sabu Siyad <hello@ssiyad.com>
2022-03-09 00:07:36 +0530

    fix: extend engagespot client instead of manual

--- lib/engagespot/engagespot.service.ts
+++ lib/engagespot/engagespot.service.ts
@@ -4,48 +4,12 @@ import { CONFIG_OPTIONS } from './const/config.const';
 import { EngagespotOptions } from './types/options.interface';
 
 @Injectable()
-export class EngagespotService {
-    private CLIENT: Engagespot;
-
+export class EngagespotService extends Engagespot {
+    /**
+     * extend and create an Engagespot client
+     * @param options module init options
+     */
     constructor(@Inject(CONFIG_OPTIONS) options: EngagespotOptions) {
-        this.CLIENT = new Engagespot(
-            options.API_KEY,
-            options.API_SECRET,
-            options.config,
-        );
-
-        this.createNotification = this.CLIENT.createNotification;
-        this.genHmac = this.CLIENT.genHmac;
-        this.connect = this.CLIENT.connect;
-        this.sendNotification = this.CLIENT.sendNotification;
+        super(options.API_KEY, options.API_SECRET, options.config);
     }
-
-    /**
-     *
-     * @param title
-     * @returns notification instance
-     */
-    createNotification;
-
-    /**
-     * returns a sha256 encoded string used to further ensure security. read more at
-     * https://documentation.engagespot.co/docs/HMAC-authentication/enabling-HMAC-authentication
-     * @param userId
-     * @returns encoded string
-     */
-    genHmac;
-
-    /**
-     * connect/register a user with Engagespot
-     * @param userId like 'hello@example.com'
-     * @returns API response
-     */
-    connect;
-
-    /**
-     * send an already prepared notification
-     * @param notification body
-     * @returns API response
-     */
-    sendNotification;
 }
